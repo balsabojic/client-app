35693848fadfc9f4fe004c5380b792a9e5d64a67
Balsa Bojic <balsha.bojic@gmail.com>
2015-10-26 10:34:58 +0100

    Village routes updated

--- routes/village.js
+++ routes/village.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/charts', function(req, res) {
-   res.render('village/charts');
+    res.render('village/charts');
+});
+
+router.get('/', function(req, res) {
+    res.render('village/index');
 });
 
 module.exports = router;
