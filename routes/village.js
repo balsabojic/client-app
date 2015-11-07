1a800209cdd796b27c955139ee36f5e7233ee93b
Balsa Bojic <balsha.bojic@gmail.com>
2015-11-07 20:41:24 +0100

    Route to the smg view added

--- routes/village.js
+++ routes/village.js
@@ -10,4 +10,8 @@ router.get('/', function(req, res) {
     res.render('village/index', {address : client.getNodeServerAddress()});
 });
 
+router.get('/smg', function(req, res) {
+	res.render('village/smg', {address : client.getNodeServerAddress()});
+});
+
 module.exports = router;
