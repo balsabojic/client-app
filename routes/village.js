3505489ac22b84b33063ecda2c54abd25ace53b3
Balsa Bojic <balsha.bojic@gmail.com>
2015-11-12 16:28:39 +0100

    SMG route deleted from the village routes

--- routes/village.js
+++ routes/village.js
@@ -10,8 +10,4 @@ router.get('/', function(req, res) {
     res.render('village/index', {address : client.getNodeServerAddress()});
 });
 
-router.get('/smg', function(req, res) {
-	res.render('village/smg', {address : client.getNodeServerAddress()});
-});
-
 module.exports = router;
